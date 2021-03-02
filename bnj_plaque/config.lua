734e0abd7f7824484194c1dbbe4838cb368ec91f
BNJ-Homme-Efficace <55634259+BNJ-Homme-Efficace@users.noreply.github.com>
2021-03-02 19:03:11 +0100

    Update config.lua

--- bnj_plaque/config.lua
+++ bnj_plaque/config.lua
@@ -1,16 +1,11 @@
 --BY BNJ-- 𝑯𝒐𝒎𝒎𝒆-𝒆𝒇𝒇𝒊𝒄𝒂𝒔𝒆
-Config = {
+ConfigPlaqueBNJ = {
 	czas = 60, -- Temps après lequel la plaque peut être de nouveau vu (minutes)
 	cooldown = 60, -- Temps d'attente pour changer de nouveau la plaque
-	money = 50000, -- Prix de changement	
+	money = 2500, -- Prix de changement	
 	coords = { x = 210.061, y = -1690.394, z = 29.55 }, -- Coordonnées de l'atelier
 
-	-- Blip (https://docs.fivem.net/game-references/blips/)
-	blipEnabled =true, -- Le blip doit-il être affiché sur la carte?
-	blipSprite = 402,
-	blipColour = 40,
-	blipScale = 0.8,
-	blipName = 'Supression de plaque',
+	
 	
 	-- Ped (https://wiki.rage.mp/index.php?title=Peds)
 	pedName = 'mp_m_waremech_01',
